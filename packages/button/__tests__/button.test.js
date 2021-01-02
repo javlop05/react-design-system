8315c204a327e92ac0f88646a1bbdc26c00a192d
javi <javilopezdelerma84@gmail.com>
2021-01-02 13:08:22 +0100

    [BBS-1][feat] Button test commented

--- packages/button/__tests__/button.test.js
+++ packages/button/__tests__/button.test.js
@@ -1,7 +1,7 @@
-'use strict';
+// 'use strict';
 
-const button = require('..');
+// const button = require('..');
 
-describe('button', () => {
-    it('needs tests');
-});
+// describe('button', () => {
+//     it('needs tests');
+// });
